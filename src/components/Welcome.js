91776f522eaff210f96f8df93b1726a468d170f8
philipwalton123 <philipwalton123@googlemail.com>
2022-06-20 13:13:27 +0100

    add demo message

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,5 +6,8 @@ export default function Welcome(){
     return <div className='welcome-wrapper'>
         <Banner />
         <Login />
+        <p>This is a portfolio project for demonstration purposes</p>
+        <p>Click guest for a preview.</p>
+        <p>To see all features as a user, login as grumpy19, tickle122, or happyamy2016</p>
     </div>
 }
